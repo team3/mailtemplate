97b56bd77240f4d528ff25953800604f44eb1ea7
Andrey Parkhomenko <andrey.parkhomenko@nextiva.com>
2018-03-25 01:07:20 +0200

    initial commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+//var elem = document.querySelector('.sidenav');
+//var instance = M.Sidenav.init(elem, options);
+
+// Initialize collapsible (uncomment the lines below if you use the dropdown variation)
+// var collapsibleElem = document.querySelector('.collapsible');
+// var collapsibleInstance = M.Collapsible.init(collapsibleElem, options);
+
+// Or with jQuery
+
+$(document).ready(function(){
+    $('.sidenav').sidenav();
+});
